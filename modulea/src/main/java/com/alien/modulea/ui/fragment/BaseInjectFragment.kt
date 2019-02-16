99b839e76925d775e375b19641175872ae7bcba7
viyski <viyski@163.com>
2019-02-16 15:25:07 +0800

    first commit

--- modulea/src/main/java/com/alien/modulea/ui/fragment/BaseInjectFragment.kt
+++ modulea/src/main/java/com/alien/modulea/ui/fragment/BaseInjectFragment.kt
@@ -1,4 +1,16 @@
 package com.alien.modulea.ui.fragment
 
-class BaseInjectFragment {
+import com.alien.base.modulekit.AModuleKit
+import com.alien.base.ui.BaseFragment
+import com.alien.modulea.di.DaggerFragmentComponent
+import com.alien.modulea.di.FragmentComponent
+import com.alien.modulea.di.ModuleAAppComponent
+
+abstract class BaseInjectFragment: BaseFragment() {
+
+    fun fragmentComponent(): FragmentComponent {
+        return DaggerFragmentComponent.builder()
+            .moduleAAppComponent(AModuleKit.getInstance().getComponent() as ModuleAAppComponent?)
+            .build()
+    }
 }
