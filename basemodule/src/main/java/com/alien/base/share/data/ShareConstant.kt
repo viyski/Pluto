de10cf2b652a8d81fbdcd1afcf98099c902c20f4
AlexReesm <viyski@163.com>
2019-02-27 16:17:01 +0800

    Create ShareConstant.kt

---
+++ basemodule/src/main/java/com/alien/base/share/data/ShareConstant.kt
@@ -0,0 +1,30 @@
+package com.alien.base.share.data
+
+object ShareConstant {
+
+    const val TYPE_LOGIN = 0
+    const val TYPE_SHARE = 1
+    const val ACTIVITY_RESULT_CODE = 1000
+    const val DATA = "data"
+
+    const val APP_NAME = ""
+
+    const val QQ_APP_ID = ""
+
+    const val QQ_SCOPE = "get_user_info,get_simple_userinfo,get_user_profile,get_app_friends," + "add_share,add_topic,list_album,upload_pic,add_album,set_user_face,get_vip_info,get_vip_rich_info,get_intimate_friends_weibo,match_nick_tips_weibo,add_pic_t"
+
+    const val SINA_APP_KEY = ""
+
+    const val SINA_APP_SECRET = ""
+
+    const val SINA_REDIRECT_URL = ""
+
+    const val SINA_SCOPE = (
+            "email,direct_messages_read,direct_messages_write,"
+                    + "friendships_groups_read,friendships_groups_write,statuses_to_me_read,"
+                    + "follow_app_official_microblog," + "invitation_write")
+
+    const val WX_APP_ID = ""
+
+    const val WX_APP_SECRET = ""
+}
